22ff52d83b0eab18d5e473a732096b3c16081136
Alice Borges <aliceborges@outlook.com.br>
2017-12-22 00:16:42 -0200

    refactor: retirando o livro do componente BookShelf e fazendo o componente Book só para ele

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import sortBy from 'sort-by'
+import Book from './Book'
 
 class BookShelf extends Component{
 
@@ -45,24 +46,9 @@ class BookShelf extends Component{
                     <div className="bookshelf-books">
                       <ol className="books-grid">
                         { category.field.map((book)=> (
-                        <li key={book.id}>
-                          <div className="book">
-                            <div className="book-top">
-                              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url('+ book.imageLinks.smallThumbnail +')' }}></div>
-                              <div className="book-shelf-changer">
-                                <select>
-                                  <option value="none" disabled>Move to...</option>
-                                  <option value="currentlyReading">Currently Reading</option>
-                                  <option value="wantToRead">Want to Read</option>
-                                  <option value="read">Read</option>
-                                  <option value="none">None</option>
-                                </select>
-                              </div>
-                            </div>
-                            <div className="book-title"> {book.title} </div>
-                            <div className="book-authors"> {book.authors} </div>
-                          </div>
-                        </li>
+                          <Book 
+                            book={book}
+                          />
                         )) }
                       </ol>
                     </div>
